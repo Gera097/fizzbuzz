f3f6a902c2335260bad88cd405a10a9db2fcb244
Gera097 <84435213+Gera097@users.noreply.github.com>
2022-04-27 15:53:25 -0500

    5.- Refactor a método getAmountOfExplorersByMission

--- test/services/ExplorerService.test.js
+++ test/services/ExplorerService.test.js
@@ -7,4 +7,9 @@ describe("Unit test for ExplorerService class", () => {
         const explorersInNode = ExplorerService.filterByMission(explorers, "node")
         expect(explorersInNode.length).toBe(2);
     }) 
+    test("Get amount of explorers by mission", () => {
+        const explorers = [{mission: "node"}, {mission: "node"}, {mission: "java"}]
+        const explorersInNode = ExplorerService.getAmountOfExplorersByMission(explorers, "node")
+        expect(explorersInNode).toBe(2);
+    }) 
 })
