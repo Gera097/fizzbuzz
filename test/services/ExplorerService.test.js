3a7c46ea8be5341be6e2ca37e3e90c193d296038
Gera097 <84435213+Gera097@users.noreply.github.com>
2022-04-27 18:57:37 -0500

    12.- Corrección de estilo con ESlint

--- test/services/ExplorerService.test.js
+++ test/services/ExplorerService.test.js
@@ -1,20 +1,20 @@
-const ExplorerService = require("./../../lib/services/ExplorerService")
+const ExplorerService = require("./../../lib/services/ExplorerService");
 
 describe("Unit test for ExplorerService class", () => {
 
     test("Filter explorers by mission", () => {
-        const explorers = [{mission: "node"}, {mission: "node"}, {mission: "java"}]
-        const explorersInNode = ExplorerService.filterByMission(explorers, "node")
+        const explorers = [{mission: "node"}, {mission: "node"}, {mission: "java"}];
+        const explorersInNode = ExplorerService.filterByMission(explorers, "node");
         expect(explorersInNode.length).toBe(2);
-    }) 
+    }); 
     test("Get amount of explorers by mission", () => {
-        const explorers = [{mission: "node"}, {mission: "node"}, {mission: "java"}]
-        const explorersInNode = ExplorerService.getAmountOfExplorersByMission(explorers, "node")
+        const explorers = [{mission: "node"}, {mission: "node"}, {mission: "java"}];
+        const explorersInNode = ExplorerService.getAmountOfExplorersByMission(explorers, "node");
         expect(explorersInNode).toBe(2);
-    }) 
+    }); 
     test("Get explorers usernames by mission", () => {
-        const explorers = [{mission: "node", "githubUsername": "ajolonauta1"}, {mission: "node", "githubUsername": "ajolonauta2"}, {mission: "java", "githubUsername": "ajolonauta3"}]
-        const explorersInNode = ExplorerService.getExplorersUsernamesByMission(explorers, "node")
+        const explorers = [{mission: "node", "githubUsername": "ajolonauta1"}, {mission: "node", "githubUsername": "ajolonauta2"}, {mission: "java", "githubUsername": "ajolonauta3"}];
+        const explorersInNode = ExplorerService.getExplorersUsernamesByMission(explorers, "node");
         expect(explorersInNode).toEqual(["ajolonauta1", "ajolonauta2"]);
-    }) 
-})
+    }); 
+});
--- test/services/FizzbuzzService.test.js
+++ test/services/FizzbuzzService.test.js
@@ -1,29 +1,29 @@
-const FizzbuzzService = require("./../../lib/services/FizzbuzzService")
+const FizzbuzzService = require("./../../lib/services/FizzbuzzService");
 
 describe("Unit testing for FizzbuzzService class", () => {
 
     test("Caso en que se muestra el score", () => {
-        const explorer1 = {name: "Explorer1", score: 1}
-        const result = FizzbuzzService.applyValidationInExplorer(explorer1)
+        const explorer1 = {name: "Explorer1", score: 1};
+        const result = FizzbuzzService.applyValidationInExplorer(explorer1);
 
-        expect(result.trick).toBe(1)
-    }) 
+        expect(result.trick).toBe(1);
+    }); 
     test("Caso en que trick es FIZZ", () => {
-        const explorer3 = {name: "Explorer3", score: 3}
-        const result = FizzbuzzService.applyValidationInExplorer(explorer3)
+        const explorer3 = {name: "Explorer3", score: 3};
+        const result = FizzbuzzService.applyValidationInExplorer(explorer3);
 
-        expect(result.trick).toBe("FIZZ")
-    }) 
+        expect(result.trick).toBe("FIZZ");
+    }); 
     test("Caso en que trick es BUZZ", () => {
-        const explorer5 = {name: "Explorer5", score: 5}
-        const result = FizzbuzzService.applyValidationInExplorer(explorer5)
+        const explorer5 = {name: "Explorer5", score: 5};
+        const result = FizzbuzzService.applyValidationInExplorer(explorer5);
 
-        expect(result.trick).toBe("BUZZ")
-    }) 
+        expect(result.trick).toBe("BUZZ");
+    }); 
     test("Caso en que trick es FIZZBUZZ", () => {
-        const explorer15 = {name: "Explorer15", score: 15}
-        const result = FizzbuzzService.applyValidationInExplorer(explorer15)
+        const explorer15 = {name: "Explorer15", score: 15};
+        const result = FizzbuzzService.applyValidationInExplorer(explorer15);
 
-        expect(result.trick).toBe("FIZZBUZZ")
-    }) 
-})
+        expect(result.trick).toBe("FIZZBUZZ");
+    }); 
+});
