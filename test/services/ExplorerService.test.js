c038908facc4c4c685385fe4c3550f474b47c02a
Gera097 <84435213+Gera097@users.noreply.github.com>
2022-04-27 16:09:12 -0500

    6.- Refactor a método getExplorersUsernamesByMission

--- test/services/ExplorerService.test.js
+++ test/services/ExplorerService.test.js
@@ -12,4 +12,9 @@ describe("Unit test for ExplorerService class", () => {
         const explorersInNode = ExplorerService.getAmountOfExplorersByMission(explorers, "node")
         expect(explorersInNode).toBe(2);
     }) 
+    test("Get explorers usernames by mission", () => {
+        const explorers = [{mission: "node", "githubUsername": "ajolonauta1"}, {mission: "node", "githubUsername": "ajolonauta2"}, {mission: "java", "githubUsername": "ajolonauta3"}]
+        const explorersInNode = ExplorerService.getExplorersUsernamesByMission(explorers, "node")
+        expect(explorersInNode).toEqual(["ajolonauta1", "ajolonauta2"]);
+    }) 
 })
