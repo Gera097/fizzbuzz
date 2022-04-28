f62119c2e5aae8874f15f416485c0a4f498442eb
Gera097 <84435213+Gera097@users.noreply.github.com>
2022-04-27 23:46:05 -0500

    22.- Enpoint para obtener usernames por misión

--- test/controllers/ExplorerController.test.js
+++ test/controllers/ExplorerController.test.js
@@ -1,21 +1,21 @@
-const ExplorerController = require("../../lib/controllers/ExplorerController")
+const ExplorerController = require("../../lib/controllers/ExplorerController");
 
 describe("Unit test for class ExplorerController", () => {
     test("Prueba método getExplorersByMission", () => {
-        const result = ExplorerController.getExplorersByMission("node")
-        console.log(result)
-        const result_java = ExplorerController.getExplorersByMission("java")
-        console.log(result)
-        expect(result_java.length).toBe(5)
-    })
+        const result = ExplorerController.getExplorersByMission("node");
+        console.log(result);
+        const result_java = ExplorerController.getExplorersByMission("java");
+        console.log(result);
+        expect(result_java.length).toBe(5);
+    });
 
     test("Prueba método getExplorersUsernamesByMission", () => {
-        const result = ExplorerController.getExplorersUsernamesByMission("node")
-        expect(result.length).toBe(10)
-    })
+        const result = ExplorerController.getExplorersUsernamesByMission("node");
+        expect(result.length).toBe(10);
+    });
 
     test("Prueba método getAmountOfExplorersByMission", () => {
-        const result = ExplorerController.getAmountOfExplorersByMission("java")
-        expect(result).toBe(5)
-    })
-})
+        const result = ExplorerController.getAmountOfExplorersByMission("java");
+        expect(result).toBe(5);
+    });
+});
