3ff340377dd4eb43e3b188adc22c06473d5e4041
Gera097 <84435213+Gera097@users.noreply.github.com>
2022-05-09 22:58:01 -0500

    30.- consulta a bot por usuarios ne misión

--- test/services/BotServices.test.js
+++ test/services/BotServices.test.js
@@ -1,10 +1,10 @@
-const BotService = require("../../lib/services/BotServices")
+const BotService = require("../../lib/services/BotServices");
 
 describe("Unit test for BotServices class", () => {
     test("Returning a String with the explorers in a mission", ()=> {
-        const explorers = [{"mission": "node", "name": "Woopa1"}, {"mission": "node", "name": "Woopa2"}, {"mission": "java", "name": "Woopa3"}]
-        const explorersInNode = BotService.getExplorersByMissionString(explorers,"node")
+        const explorers = [{"mission": "node", "name": "Woopa1"}, {"mission": "node", "name": "Woopa2"}, {"mission": "java", "name": "Woopa3"}];
+        const explorersInNode = BotService.getExplorersByMissionString(explorers,"node");
 
-        expect(explorersInNode).toMatch(`[{"mission":"node","name":"Woopa1"},{"mission":"node","name":"Woopa2"}]`)
-    })
-})
+        expect(explorersInNode).toMatch("[{\"mission\":\"node\",\"name\":\"Woopa1\"},{\"mission\":\"node\",\"name\":\"Woopa2\"}]");
+    });
+});
